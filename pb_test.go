8a37d49e502e20ad0adef3cbc1a40766a5ac3ed2
beka birhanu <beka.b.atomsa@gmail.com>
2025-02-08 09:21:00 +0300

    test: add action encoder unit test

---
+++ pb_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+)
+
+func TestGamePb(t *testing.T) {
+	t.Run("GamePb_Marshal_Unmarshal_Action", testMarshalUnmarshalAction)
+}
+
+func testMarshalUnmarshalAction(t *testing.T) {
+	id := uuid.New()
+	encoder := &Protobuf{}
+	action := encoder.NewAction()
+	action.SetDirection("North")
+	action.SetID(id)
+
+	if action.GetID() == uuid.Nil {
+		t.Errorf("Expected ID: %s, got %s", id, action.GetID())
+	}
+	marshaledAction, err := encoder.MarshalAction(action)
+	if err != nil {
+		t.Errorf("Expected marshal, got error: %s", err)
+		t.FailNow()
+	}
+
+	unmarshaledAction, err := encoder.UnmarshalAction(marshaledAction)
+	if err != nil {
+		t.Errorf("Expected unmarshal, got error: %s", err)
+		t.FailNow()
+	}
+
+	if unmarshaledAction.GetID() != action.GetID() {
+		t.Errorf("Expected ID: %s , got : %s", action.GetID(), unmarshaledAction.GetID())
+		t.FailNow()
+	}
+}
